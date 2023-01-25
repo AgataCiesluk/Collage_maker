dfaf99c8f682e1cf5ee3437b09987008061d9af3
Agatka <agata.mikla@gmail.com>
2023-01-25 22:29:32 +0100

    Fix main.py based on comments from PR#1

--- main.py
+++ main.py
@@ -3,23 +3,28 @@ import urllib.request
 
 import requests
 from dotenv import load_dotenv
+from json.decoder import JSONDecodeError
+
+load_dotenv()
 
 if __name__ == '__main__':
 
     topic = 'cat'
 
-    load_dotenv()
     API_ACCESS_KEY = os.getenv('API_ACCESS_KEY')
 
     response = requests.get(f'https://api.unsplash.com/search/photos?client_id={API_ACCESS_KEY}&page=1&query={topic}')
 
-    data = {}
     if response.status_code == 200:
         data = response.json()
+        if not data:
+            raise JSONDecodeError
 
-    urls_small_photos = []
-    for element in data['results']:
-        urls_small_photos.append(element['urls']['small'])
+        urls_small_photos = []
+        for element in data['results']:
+            urls_small_photos.append(element['urls']['small'])
 
-    for index, url in enumerate(urls_small_photos):
-        urllib.request.urlretrieve(url, f'photos/cat{index}.png')
+        for index, url in enumerate(urls_small_photos):
+            urllib.request.urlretrieve(url, f'photos/cat{index}.png')
+    else:
+        response.raise_for_status()
