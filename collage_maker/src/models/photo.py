c5e2fec9d6a2774501a3548623678ea594338b9f
Agatka <agata.mikla@gmail.com>
2023-02-25 21:27:30 +0100

    Add Photo class draft

--- collage_maker/src/models/photo.py
+++ collage_maker/src/models/photo.py
@@ -2,5 +2,15 @@ from PIL import Image
 
 
 class Photo(Image):
-    def __init__(self):
-        pass
+    path: str
+
+    def __init__(self, path):
+        self.path = path
+
+    @property
+    def width(self):
+        return self.width
+
+    @property
+    def height(self):
+        return self.height
